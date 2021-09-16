52fdce06f41ad023330c3152daf03c1aee79ccd0
Oscar Alvarez <oscar.alvarez@carsales.com>
2021-09-16 12:41:31 -0300

    lifecycle router

---
+++ routercovidmodule/src/main/java/com/example/routercovidmodule/lifecycle/CovidApiReceivedServiceImpl.kt
@@ -0,0 +1,6 @@
+package com.example.routercovidmodule.lifecycle
+
+import com.example.covidapiservicemodule.service.ICovidApiReceivedService
+
+class CovidApiReceivedServiceImpl: ICovidApiReceivedService {
+}
--- routercovidmodule/src/main/java/com/example/routercovidmodule/lifecycle/GlobalCovidRouterServiceLifeCycle.kt
+++ routercovidmodule/src/main/java/com/example/routercovidmodule/lifecycle/GlobalCovidRouterServiceLifeCycle.kt
@@ -1,6 +1,18 @@
 package com.example.routercovidmodule.lifecycle
 
 import android.app.Application
+import au.com.carsales.basemodule.router.BaseModuleLifeCycle
+import au.com.carsales.basemodule.router.IModuleConfig
+import com.example.covidapiservicemodule.service.CovidApiServiceImpl
+import com.example.covidapiservicemodule.service.ICovidApiReceivedService
+import com.example.covidapiservicemodule.service.ICovidApiService
 
-class GlobalCovidRouterServiceLifeCycle(application: Application){
+class GlobalCovidRouterServiceLifeCycle(application: Application): BaseModuleLifeCycle(application){
+
+    override fun onCreate(config: IModuleConfig) {
+        config.registerService(ICovidApiService::class.java, CovidApiServiceImpl::class.java)
+        config.registerService(ICovidApiReceivedService::class.java, CovidApiReceivedServiceImpl::class.java)
+
+    }
+
 }
